14dc7e79fb8e5edbcaf16ab46e2a8576a845f853
anmo <anmoanmomo@gmail.com>
2023-09-28 21:52:36 +0800

    2023/9/28  21：52

--- week4/week4/H.cpp
+++ week4/week4/H.cpp
@@ -1,40 +1,40 @@
-//#include <iostream>
-//#include <string>
-//
-//using namespace std;
-//
-//bool judge(const string& s) {
-//    long a = 0;
-//    for (long i = 0; i < s.length(); i++) {
-//        if (s[i] == '(' || s[i] == '[') {
-//            a++;
-//        }
-//        else if (s[i] == ')' || s[i] == ']') {
-//            if (a <= 0) {
-//                return false;
-//            }
-//            a--;
-//                if ((s[i] == ')' && s[a] != '(') || (s[i] == ']' && s[a] != '[')) {
-//                    return false;
-//                }
-//        }
-//    }
-//    return true;
-//}
-//
-//int main() {
-//    string str;
-//    while (cin >> str) {
-//        bool result = judge(str);
-//        if (result) {
-//            cout << "YES" << endl;
-//        }
-//        else {
-//            cout << "NO" << endl;
-//        }
-//    }
-//    return 0;
-//}
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool judge(const string& s) {
+    long a = 0;
+    for (long i = 0; i < s.length(); i++) {
+        if (s[i] == '(' || s[i] == '[') {
+            a++;
+        }
+        else if (s[i] == ')' || s[i] == ']') {
+            if (a <= 0) {
+                return false;
+            }
+            a--;
+                if ((s[i] == ')' && s[a] != '(') || (s[i] == ']' && s[a] != '[')) {
+                    return false;
+                }
+        }
+    }
+    return true;
+}
+
+int main() {
+    string str;
+    while (cin >> str) {
+        bool result = judge(str);
+        if (result) {
+            cout << "YES" << endl;
+        }
+        else {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
 // 
 // 
 // 
--- week4/week4/I.cpp
+++ week4/week4/I.cpp
@@ -1,50 +1,155 @@
-#include <iostream>
-#include <string>
-#include <vector>
-using namespace std;
+//#include <iostream>
+//#include <string>
+//#include <vector>
+//using namespace std;
+//
+//bool canLeave(const string& a, const string& b, vector<char>& stack, string& output) {
+//    int i1 = 0, i2 = 0; // O1和O2的索引
+//    while (i2 < a.length()) {
+//        // 如果栈为空或者栈顶元素不等于b的当前字符，尝试将a的字符入栈
+//        if (stack.empty() || stack.back() != b[i2]) {
+//            if (i1 >= a.length()) {
+//                return false; //防止越界
+//            }
+//            output += "in ";
+//            stack.push_back(a[i1]);
+//            i1++;
+//        }
+//        else {
+//            output += "out ";
+//            stack.pop_back();
+//            i2++;
+//        }
+//    }
+//    return true;
+//}
+//
+//int main() {
+//    int n;
+//    while (cin >> n) {
+//        if (n == 0) {
+//            break;
+//        }
+//        string a, b;
+//        cin >> a >> b;
+//        vector<char> v;
+//        string output;
+//
+//        if (canLeave(a, b, v, output)) {
+//            cout << "Yes." << endl;
+//            cout << output << endl;
+//        }
+//        else {
+//            cout << "No." << endl;
+//        }
+//
+//        cout << "FINISH" << endl;
+//    }
+//    return 0;
+//}
 
-bool canLeave(const string& a, const string& b, vector<char>& stack, string& output) {
-    int i1 = 0, i2 = 0; // O1和O2的索引
-    while (i2 < a.length()) {
-        // 如果栈为空或者栈顶元素不等于b的当前字符，尝试将a的字符入栈
-        if (stack.empty() || stack.back() != b[i2]) {
-            if (i1 >= a.length()) {
-                return false; // a已经遍历完，但是无法匹配b
-            }
-            output += "in ";
-            stack.push_back(a[i1]);
-            i1++;
-        }
-        else {
-            // 栈顶元素等于b的当前字符，尝试将其出栈
-            output += "out ";
-            stack.pop_back();
-            i2++;
-        }
-    }
-    return true;
-}
+// 
+//#include <iostream>
+//#include <list>
+//#include <string>
+//
+//using namespace std;
+//
+//bool judge( list<int>& a,  list<int>& b) {
+//    list<int> station; // 用于模拟列车站台的栈
+//
+//    for (const int& train : a) {
+//        station.push_back(train); // 列车进站
+//        cout << "in" << endl;
+//
+//        while (!station.empty() && station.back() == b.front()) {
+//            station.pop_back(); // 列车出站
+//            b.pop_front(); // 更新出站顺序
+//            cout << "out" << endl;
+//        }
+//    }
+//
+//    return station.empty(); // 如果所有列车都出站，返回true
+//}
+//
+//int main() {
+//    int n = 0;
+//    while (cin >> n) {
+//        if (n == 0) break;
+//
+//        list<int> a, b;
+//        for (int i = 0; i < n; i++) {
+//            int x = 0; cin >> x;
+//            a.push_back(x);
+//        }
+//        for (int i = 0; i < n; i++) {
+//            int x = 0; cin >> x;
+//            b.push_back(x);
+//        }
+//
+//        if (judge(a, b)) {
+//            cout << "Yes." << endl;
+//        }
+//        else {
+//            cout << "No." << endl;
+//        }
+//
+//        cout << "FINISH" << endl;
+//    }
+//    return 0;
+//}
 
-int main() {
-    int n;
-    while (cin >> n) {
-        if (n == 0) {
-            break;
-        }
-        string a, b;
-        cin >> a >> b;
-        vector<char> v;
-        string output;
-
-        if (canLeave(a, b, v, output)) {
-            cout << "Yes." << endl;
-            cout << output << endl;
-        }
-        else {
-            cout << "No." << endl;
-        }
-
-        cout << "FINISH" << endl;
-    }
-    return 0;
-}
+//
+//#include<iostream>
+//#include<stack>
+//#include<vector>
+//#include<string>
+//#include<algorithm>
+//
+//using namespace std;
+//
+//bool judge(string& a, string& b, string& out) {
+//	int index1 = 0, index2 = 0;
+//	stack<int> s;
+//	while (index2 < a.length()) {
+//		if (s.empty() || s.top() != b[index2]) {
+//			if (index1 >= a.length()) {
+//				return false;
+//			}
+//			else {
+//				s.push(a[index1]);
+//				index1++;
+//				out += "in\n";
+//			}
+//		}
+//		else {
+//			s.pop();
+//			index2++;
+//			out += "out\n";
+//		}
+//
+//	}
+//	return true;
+//}
+//
+//int main() {
+//	while (1) {
+//		int n = 0;
+//		cin >> n;
+//		while (n--) {
+//			string a, b;
+//			cin >> a >> b;
+//			string out;
+//
+//			if (judge(a, b, out)) {
+//				cout << "Yes." << endl;
+//				cout << out << endl;
+//				cout << "FINISH" << endl;
+//			}
+//			else {
+//				cout << "No." << endl;
+//				cout << "FINISH" << endl;
+//			}
+//		}
+//	}
+//}
