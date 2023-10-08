86ab8cc26c8cbc321a0fe82f74b4abb4cbc9c3c6
anmo <anmoanmomo@gmail.com>
2023-10-08 20:34:26 +0800

    2023/10/8 20：34

---
+++ week6/week6/A.cpp
@@ -0,0 +1,22 @@
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//
+//int main() {
+//	int t = 0; cin >> t;
+//	while (t--) {
+//		int n = 0; cin >> n;
+//		string space[200];
+//		string star[200];
+//		for (int i = 0; i < n; i++) {
+//			space[i] = string(n - i - 1, ' ');
+//			star[i] = string(i + 1, '*');
+//		}
+//		for (int i = 0; i < n; i++) {
+//			cout << space[i] << star[i] << endl;
+//		}
+//	}
+//	return 0;
+//}
+
+
