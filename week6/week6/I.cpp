70e70429c7b81213b21a27489df942705fe841b5
anmo <anmoanmomo@gmail.com>
2023-10-13 16:54:20 +0800

    2023/10/13 16：54

---
+++ week6/week6/I.cpp
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<cmath>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<iomanip>
+#include<stack>
+
+#define endl '\n';
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+
+
+
+	return 0;
+}
