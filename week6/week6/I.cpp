149af1310fe9092f37459b982d31d0846ad9b8cc
anmo <anmoanmomo@gmail.com>
2023-10-16 22:11:55 +0800

    2023/10/16 22：11

--- week6/week6/I.cpp
+++ week6/week6/I.cpp
@@ -77,3 +77,82 @@
 //    return 0;
 //}
 
+#include<iostream>
+#define endl '\n'
+using namespace std;
+const int N = 107;
+
+int dx[] = { 1,0,-1,0 }; //表示东南西北方向的模拟
+int dy[] = { 0,-1,0,1 };
+struct robot  // 机器人 
+{
+	int x, y;
+	int direction;
+}r[N];
+int main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int k;
+	cin >> k;
+	while (k--)
+	{
+		int a, b;
+		cin >> a >> b;//  a为EW方向    b为NS方向 
+		int n, m;
+		cin >> n >> m;//  n为机器人数量    m为指令 
+		for (int i = 1; i <= n; i++)
+		{
+			char d;
+			cin >> r[i].x >> r[i].y >> d;
+			if (d == 'E') r[i].direction = 0;
+			if (d == 'S') r[i].direction = 1;
+			if (d == 'W') r[i].direction = 2;
+			if (d == 'N') r[i].direction = 3;
+		}
+		bool crash = false;
+		for (int i = 1; i <= m; i++)
+		{
+			int num;
+			char action;
+			int repetition;
+			cin >> num >> action >> repetition; 
+			if (crash) {
+				continue;
+			}
+			while (repetition-- && !crash)
+			{
+				if (action == 'L') r[num].direction = (r[num].direction + 3) % 4;
+				if (action == 'R') r[num].direction = (r[num].direction + 1) % 4;
+				if (action == 'F')
+				{
+					int rx = r[num].x + dx[r[num].direction];
+					int ry = r[num].y + dy[r[num].direction];
+					for (int j = 1; j <= n; j++)
+					{
+						if (j != num && rx == r[j].x && ry == r[j].y)
+						{
+							cout << "Robot " << num << " crashes into robot " << j << endl;
+							crash = true;
+						}
+					}
+					if (rx<1 || rx>a || ry<1 || ry>b)
+					{
+						cout << "Robot " << num << " crashes into the wall" << endl;
+						crash = true;
+						break;
+					}
+					if (!crash)
+					{
+						r[num].x = rx;
+						r[num].y = ry;
+					}
+				}
+			}
+		}
+		if (!crash)
+		{
+			cout << "OK" << endl;
+		}
+	}
+	return 0;
+}
--- week6/week6/Q.cpp
+++ week6/week6/Q.cpp
@@ -79,74 +79,76 @@
 //
 //    return 0;
 //}
-#define _CRT_SECURE_NO_WARNINGS
-#include<iostream>
-#include<cmath>
-#include<string>
-#include<vector>
-#include<queue>
-#include<algorithm>
-#include<iomanip>
-
-using namespace std;
-
-struct cow {
-    int start;  // 开始时间
-    int end;    // 结束时间
-    int id;     // 奶牛的编号
-};
-
-struct stall {
-    int end;    // 牛棚的结束时间
-    int id;     // 牛棚的编号
-};
-
-bool operator<(const stall& a, const stall& b) {
-    return a.end > b.end;
-}
-
-bool compare(const cow& a, const cow& b) {
-    return a.start < b.start;
-}
-
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    int t;
-    cin >> t;
-
-    vector<cow> cows(t);
-    for (int i = 0; i < t; i++) {
-        cin >> cows[i].start >> cows[i].end;
-        cows[i].id = i;
-    }
-    sort(cows.begin(), cows.end(), compare);
-
-    priority_queue<stall> pq;
-    vector<int> cow_stall(t);
-    int stallcount = 0;
-
-    for (int i = 0; i < t; i++) {
-        if (!pq.empty() && pq.top().end < cows[i].start) {
-            cow_stall[cows[i].id] = pq.top().id;
-            stall temp = pq.top();
-            pq.pop();
-            temp.end = cows[i].end;
-            pq.push(temp);
-        }
-        else {
-            stallcount++;
-            cow_stall[cows[i].id] = stallcount;
-            pq.push(stall{ cows[i].end, stallcount });  
-        }
-    }
-
-    cout << stallcount << endl;
-    for (int i = 0; i < t; i++) {
-        cout << cow_stall[i] << endl;
-    }
-
-    return 0;
-}
+// 
+// 使用优先队列减低时间复杂度
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<iostream>
+//#include<cmath>
+//#include<string>
+//#include<vector>
+//#include<queue>
+//#include<algorithm>
+//#include<iomanip>
+//
+//using namespace std;
+//
+//struct cow {
+//    int start;  // 开始时间
+//    int end;    // 结束时间
+//    int id;     // 奶牛的编号
+//};
+//
+//struct stall {
+//    int end;    // 牛棚的结束时间
+//    int id;     // 牛棚的编号
+//};
+//
+//bool operator<(const stall& a, const stall& b) {
+//    return a.end > b.end;
+//}
+//
+//bool compare(const cow& a, const cow& b) {
+//    return a.start < b.start;
+//}
+//
+//int main() {
+//    ios::sync_with_stdio(false);
+//    cin.tie(NULL);
+//    cout.tie(NULL);
+//
+//    int t;
+//    cin >> t;
+//
+//    vector<cow> cows(t);
+//    for (int i = 0; i < t; i++) {
+//        cin >> cows[i].start >> cows[i].end;
+//        cows[i].id = i;
+//    }
+//    sort(cows.begin(), cows.end(), compare);
+//
+//    priority_queue<stall> pq;
+//    vector<int> cow_stall(t);
+//    int stallcount = 0;
+//
+//    for (int i = 0; i < t; i++) {
+//        if (!pq.empty() && pq.top().end < cows[i].start) {
+//            cow_stall[cows[i].id] = pq.top().id;
+//            stall temp = pq.top();
+//            pq.pop();
+//            temp.end = cows[i].end;
+//            pq.push(temp);
+//        }
+//        else {
+//            stallcount++;
+//            cow_stall[cows[i].id] = stallcount;
+//            pq.push(stall{ cows[i].end, stallcount });  
+//        }
+//    }
+//
+//    cout << stallcount << endl;
+//    for (int i = 0; i < t; i++) {
+//        cout << cow_stall[i] << endl;
+//    }
+//
+//    return 0;
+//}
