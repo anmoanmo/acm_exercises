692b8a14c8064f0cb74167f20b65681fe7f223bb
anmo <anmoanmomo@gmail.com>
2024-04-23 20:18:11 +0800

    2024/4/23 20:18

--- lanqiao/ALDS/Algorithms and Data Structures I/1_A_Insertion Sort.py
+++ lanqiao/ALDS/Algorithms and Data Structures I/1_A_Insertion Sort.py
@@ -5,3 +5,14 @@
 @IDE:PyCharm
 =============================
 """
+n = int(input())
+a = list(map(int, input().split()))
+print(' '.join(map(str, a)))
+for i in range(1,len(a)):
+    key = a[i]
+    j = i-1
+    while j >= 0 and a[j] > key:
+        a[j+1] = a[j]
+        j -= 1
+    a[j+1] = key
+    print(' '.join(map(str, a)))
